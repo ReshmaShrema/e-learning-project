f5371681e43c312249b867800c6c1503bb41e3db
ReshmaShrema <shremareshma@gmail.com>
2023-03-20 10:36:10 +0530

    project setup completed

---
+++ client/_app.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/reset.css';
+
+function MyApp({Component,pageProps}){
+    return <Component{...pageProps}/>
+}
+export default MyApp;
